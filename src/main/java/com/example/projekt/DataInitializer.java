d20bb201658278fbc16bc566ce69fd8d62805006
Kazek360 <kazimierz.piontek@gmail.com>
2024-06-15 23:42:49 +0200

    FireOrders

--- src/main/java/com/example/projekt/DataInitializer.java
+++ src/main/java/com/example/projekt/DataInitializer.java
@@ -2,14 +2,15 @@ package com.example.projekt;
 
 import com.example.projekt.model.ArtillerySite;
 import com.example.projekt.model.Enums.ArtillerySiteState;
+import com.example.projekt.model.FireOrder;
 import com.example.projekt.model._Employee;
-import com.example.projekt.repository.ArtillerySiteRepository;
-import com.example.projekt.repository._EmployeeRepository;
+import com.example.projekt.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 @Component
@@ -18,6 +19,9 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
 
     private final _EmployeeRepository empRepo;
     private final ArtillerySiteRepository artillerySiteRepository;
+    private final FireOrderRepository fireOrderRepository;
+    private final ArtillerySite_SupplyStationRepository artillerySupplyRepository;
+    private final SupplyStationRepository supplyStationRepository;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -26,7 +30,7 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
 
     private void initData() {
         boolean employeeExists = empRepo.count() > 0;
-        if(!employeeExists) {
+        if (!employeeExists) {
 
             _Employee e1 = _Employee.builder()
                     .firstName("Jan")
@@ -41,7 +45,7 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
 
         }
         boolean artilleryExists = artillerySiteRepository.count() > 0;
-        if (!artilleryExists){
+        if (!artilleryExists) {
             ArtillerySite artillerySite1 = ArtillerySite.builder()
                     .cannons(20)
                     .ammunition(15)
@@ -55,7 +59,26 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
                     .location("Gdańsk")
                     .artillerySiteState(ArtillerySiteState.WAITING_FOR_AMMUNITION)
                     .build();
-            artillerySiteRepository.saveAll(Arrays.asList(artillerySite1,artillerySite2));
+            artillerySiteRepository.saveAll(Arrays.asList(artillerySite1, artillerySite2));
+        }
+        boolean fireOrderExists = fireOrderRepository.count() > 0;
+        if (!fireOrderExists) {
+            FireOrder fireOrder1 = FireOrder.builder()
+                    .artilleryBarageLocation("Warszawa")
+                    .duration(2)
+                    .date(LocalDate.now()
+                    .plusYears(200))
+                    .artillerySite(artillerySiteRepository.findById(1L).get())
+                    .build();
+
+            FireOrder fireOrder2 = FireOrder.builder()
+                    .artilleryBarageLocation("Warszawa")
+                    .duration(5)
+                    .date(LocalDate.now()
+                    .plusYears(150))
+                    .artillerySite(artillerySiteRepository.findById(1L).get())
+                    .build();
+            fireOrderRepository.saveAll(Arrays.asList(fireOrder1, fireOrder2));
         }
     }
 
