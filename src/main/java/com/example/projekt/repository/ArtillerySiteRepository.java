e2134df70fe25f286912c382a6e026b3b8de496d
Kazek360 <kazimierz.piontek@gmail.com>
2024-06-16 13:59:44 +0200

    Update ammo

--- src/main/java/com/example/projekt/repository/ArtillerySiteRepository.java
+++ src/main/java/com/example/projekt/repository/ArtillerySiteRepository.java
@@ -1,7 +1,17 @@
 package com.example.projekt.repository;
 
 import com.example.projekt.model.ArtillerySite;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ArtillerySiteRepository extends CrudRepository<ArtillerySite, Long> {
+
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE ArtillerySite a SET a.ammunition = :newAmmo WHERE a.id = :artillerySiteId")
+    void updateAmmunition(@Param("artillerySiteId") Long artillerySiteId, @Param("newAmmo") int newAmmo);
 }
--- src/main/java/com/example/projekt/repository/SupplyStationRepository.java
+++ src/main/java/com/example/projekt/repository/SupplyStationRepository.java
@@ -1,7 +1,16 @@
 package com.example.projekt.repository;
 
 import com.example.projekt.model.SupplyStation;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface SupplyStationRepository extends CrudRepository<SupplyStation, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE SupplyStation s SET s.ammunition = :newAmmo WHERE s.id = :supplyStationId")
+    void updateAmmunition(@Param("supplyStationId") Long supplyStationId, @Param("newAmmo") int newAmmo);
 }
